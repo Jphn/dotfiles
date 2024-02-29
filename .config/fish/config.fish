747f4df9283f28cf51ebf571855b2044617e9a0a
Jphn <joaopedroholandaneves@gmail.com>
2024-02-29 16:43:12 -0300

    feat(fish): enable ls alias

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -26,7 +26,7 @@ alias ld='eza -lD'
 alias lf='eza -lF --color=always | grep -v /'
 alias lh='eza -dl .* --group-directories-first'
 alias ll='eza -al --group-directories-first'
-# alias ls='eza -alF --color=always --sort=size | grep -v /'
+alias ls='eza -alF --color=always --sort=size | grep -v /'
 alias lt='eza -al --sort=modified'
 
 alias freload='. ~/.config/fish/config.fish'
