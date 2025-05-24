44e461a7d93112bfb67f8cf1528397acdfc4a63b
Jphn <joaopedroholandaneves@gmail.com>
2025-05-24 16:31:26 -0300

    feat(nvim/config): activate vim spellcheck

--- .config/nvim/lua/config/options.lua
+++ .config/nvim/lua/config/options.lua
@@ -6,3 +6,5 @@ vim.opt.tabstop = 4 -- A TAB character looks like 4 spaces
 vim.opt.expandtab = false -- Pressing the TAB key will insert spaces instead of a TAB character
 vim.opt.softtabstop = 4 -- Number of spaces inserted instead of a TAB character
 vim.opt.shiftwidth = 4 -- Number of spaces inserted when indenting
+vim.opt.spell = true
+vim.opt.spelllang = { "en_us", "pt_br" }
