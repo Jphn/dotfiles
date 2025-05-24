9e9384a9ffd9c27d7404bc3937644444dcaea533
Jphn <joaopedroholandaneves@gmail.com>
2025-05-24 16:29:26 -0300

    fix(nvim/plugins): update conform settings

--- .config/nvim/lua/plugins/conform.lua
+++ .config/nvim/lua/plugins/conform.lua
@@ -10,72 +10,64 @@ return {
 		{
 			"<leader>cF",
 			function()
-				require("conform").format({ formatters = { "injected" } })
+				require("conform").format({ formatters = { "injected" }, timeout_ms = 3000 })
 			end,
 			mode = { "n", "v" },
 			desc = "Format Injected Langs",
 		},
 	},
-	-- init = function()
-	--   -- Install the conform formatter on VeryLazy
-	--   require("lazyvim.util").on_very_lazy(function()
-	--     require("lazyvim.util").format.register({
-	--       name = "conform.nvim",
-	--       priority = 100,
-	--       primary = true,
-	--       format = function(buf)
-	--         local plugin = require("lazy.core.config").plugins["conform.nvim"]
-	--         local Plugin = require("lazy.core.plugin")
-	--         local opts = Plugin.values(plugin, "opts", false)
-	--         require("conform").format(Util.merge(opts.format, { bufnr = buf }))
-	--       end,
-	--       sources = function(buf)
-	--         local ret = require("conform").list_formatters(buf)
-	--         ---@param v conform.FormatterInfo
-	--         return vim.tbl_map(function(v)
-	--           return v.name
-	--         end, ret)
-	--       end,
-	--     })
-	--   end)
-	-- end,
+	init = function()
+		-- Install the conform formatter on VeryLazy
+		LazyVim.on_very_lazy(function()
+			LazyVim.format.register({
+				name = "conform.nvim",
+				priority = 100,
+				primary = true,
+				format = function(buf)
+					require("conform").format({ bufnr = buf })
+				end,
+				sources = function(buf)
+					local ret = require("conform").list_formatters(buf)
+					---@param v conform.FormatterInfo
+					return vim.tbl_map(function(v)
+						return v.name
+					end, ret)
+				end,
+			})
+		end)
+	end,
 	opts = function()
-		-- local plugin = require("lazy.core.config").plugins["conform.nvim"]
+		local plugin = require("lazy.core.config").plugins["conform.nvim"]
 		-- if plugin.config ~= M.setup then
-		--   Util.error({
-		--     "Don't set `plugin.config` for `conform.nvim`.\n",
-		--     "This will break **LazyVim** formatting.\n",
-		--     "Please refer to the docs at https://www.lazyvim.org/plugins/formatting",
-		--   }, { title = "LazyVim" })
+		-- 	LazyVim.error({
+		-- 		"Don't set `plugin.config` for `conform.nvim`.\n",
+		-- 		"This will break **LazyVim** formatting.\n",
+		-- 		"Please refer to the docs at https://www.lazyvim.org/plugins/formatting",
+		-- 	}, { title = "LazyVim" })
 		-- end
-		---@class ConformOpts
+		---@type conform.setupOpts
 		local opts = {
-			-- LazyVim will use these options when formatting with the conform.nvim formatter
 			default_format_opts = {
 				timeout_ms = 3000,
 				async = false, -- not recommended to change
 				quiet = false, -- not recommended to change
+				lsp_format = "fallback", -- not recommended to change
 			},
-			---@type table<string, conform.FormatterUnit[]>
 			formatters_by_ft = {
 				lua = { "stylua" },
 				fish = { "fish_indent" },
 				sh = { "shfmt" },
-				["typescript"] = { "prettierd", "deno_fmt" },
-				["javascript"] = { "prettierd" },
-				["markdown"] = { "prettierd" },
-				["json"] = { "prettierd" },
+				["typescript"] = { "biome" },
+				["javascript"] = { "biome" },
+				["typescriptreact"] = { "biome" },
+				["markdown"] = { "biome" },
+				["json"] = { "biome" },
 			},
 			-- The options you set here will be merged with the builtin formatters.
 			-- You can also define any custom formatters here.
 			---@type table<string, conform.FormatterConfigOverride|fun(bufnr: integer): nil|conform.FormatterConfigOverride>
 			formatters = {
 				injected = { options = { ignore_errors = true } },
-				deno_fmt = {
-					condition = function(ctx)
-						return vim.fs.find({ "deno.json" }, { path = ctx.filename, upward = true })[1]
-					end,
-				},
 				-- # Example of using dprint only when a dprint.json file is present
 				-- dprint = {
 				--   condition = function(ctx)
@@ -91,5 +83,4 @@ return {
 		}
 		return opts
 	end,
-	config = M.setup,
 }
